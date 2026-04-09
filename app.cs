fd81fee7d7c34e838b77d56692bc77a933df508f
Alisapipisa <lisasmo@icloud.com>
2026-04-09 18:51:37 +0500

    Добавила метод Subtract

---
+++ app.cs
@@ -0,0 +1,13 @@
+    using System;
+   class App {
+
+    static int Subtract(int a, int b;)
+    {return a-b;}
+
+       static void Main() {
+           Console.WriteLine("Калькулятор");
+           Console.WriteLine("Разность: ")
+           Console.Subtract(30, 12);
+
+       }
+   }
