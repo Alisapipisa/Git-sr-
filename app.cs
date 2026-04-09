0cdb1720dc5a4aae70b1a91d7ed5103e101b6c16
Alisapipisa <lisasmo@icloud.com>
2026-04-09 19:22:01 +0500

    метод умножение

--- app.cs
+++ app.cs
@@ -2,13 +2,19 @@
 using System;
    class App {
 
+
     static int Subtract(int a, int b;)
     {return a-b;}
+
+    static int Multiply(int a, int b)
+    {return a*b;}
 
        static void Main() {
            Console.WriteLine("Калькулятор v2.0-FINAL");
            Console.WriteLine("Разность: ")
            Console.Subtract(30, 12);
+           Console.WriteLine("Умножение: ");
+           Console.Multiply(30, 12);
 
        }
    }
